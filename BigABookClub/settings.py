22e50f80fb7bfd4bc742f264017a273a0f348606
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-10-21 17:06:35 -0400

    Changing psycopg version and updated django secrets env variable

--- BigABookClub/settings.py
+++ BigABookClub/settings.py
@@ -33,7 +33,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
