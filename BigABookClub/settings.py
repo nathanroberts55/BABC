97d0e6207d252b1d921ba5a79e79fa262116c91b
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-10-21 16:40:40 -0400

    Matching Tutorial Files

--- BigABookClub/settings.py
+++ BigABookClub/settings.py
@@ -40,6 +40,10 @@ DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 ALLOWED_HOSTS = []
 
+if "CODESPACE_NAME" in os.environ:
+    CSRF_TRUSTED_ORIGINS = [
+        f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}'
+    ]
 
 # Application definition
 
