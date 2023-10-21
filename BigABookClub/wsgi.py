1a345eb9555cdf655c870fd15a03d34c2689ddea
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-10-21 16:19:14 -0400

    Was missing wsgi settings

--- BigABookClub/wsgi.py
+++ BigABookClub/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BigABookClub.settings')
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = (
+    "BigABookClub.production"
+    if "WEBSITE_HOSTNAME" in os.environ
+    else "BigABookClub.settings"
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
