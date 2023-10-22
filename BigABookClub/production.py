ed575a1e52149484eaa2c7dc4cb808a91bb2d263
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-10-21 20:12:26 -0400

    Removed whitenoise nostatic from installed apps

--- BigABookClub/production.py
+++ BigABookClub/production.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "whitenoise.runserver_nostatic",
     "compressor",
     "home",
     "books",
