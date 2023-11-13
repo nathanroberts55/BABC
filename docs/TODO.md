d9460de09e7e4ed322c72953f6b4fafbada3f2fd
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-11-13 18:22:01 -0500

    Updated TODO with new ideas. Changed Recommendations Input and added Search

--- docs/TODO.md
+++ docs/TODO.md
@@ -4,6 +4,12 @@
   - Author
   - Title
 
+# Recommendation Search
+Search bar that filters the recommendations 
+- [ ] Filter By Book Title
+- [ ] Filter By Book Author
+- [ ] Filter By Submitter?
+
 # Update Form Validation for Youtube
 Stream VODs on Twitch have an expiration date, so need way to accept youtube links
 - [ ] Accept Youtube Links
@@ -11,3 +17,21 @@ Stream VODs on Twitch have an expiration date, so need way to accept youtube lin
   - [ ] Validate against Atrioc Clips Channel
   - [ ] Validate against Atrioc Main channel
 - [ ] Accept Links to Atroic's Twitter
+
+# Book Details Page
+Ability to click on Recommendation to get details on Individual Book
+- [ ] URL for Book Details
+- [ ] View for Book Details
+- [ ] UI for Book Details
+- [ ] Database Fields for Book Details
+  - [ ] Slug?: str
+  - [ ] Description: str
+  - [ ] Comments: str (Comments Table)
+
+# Book Tagging
+Books have tags based on topic/genre (for filtering)
+- [ ] Does API Support topics/subjects/genre?
+- [ ] Topic Tagging (Database)
+- [ ] Genre Tagging (Database)
+- [ ] Tags (UI Element)
+  
--- static/js/recommendation_filtering.js
+++ static/js/recommendation_filtering.js
@@ -1,4 +1,5 @@
 const selectElement = document.getElementById('recommendations-select');
+const recommendationSearchBar = document.getElementById('recommendationSearch');
 
 const checkSelectValue = (value) => {
 	const books = document.querySelectorAll('div.book-recommendation');
@@ -21,9 +22,43 @@ const checkSelectValue = (value) => {
 	}
 };
 
+const filterRecommendations = (value) => {
+	const searchKeyValue = document.getElementById(
+		'recommendationSearchKey'
+	).value;
+	const sourceValue = selectElement.value;
+	const books = document.querySelectorAll('div.book-recommendation');
+	books.forEach((book) => {
+		if (book.getAttribute('data-source') !== sourceValue) {
+			book.style.display = 'none';
+		} else if (
+			searchKeyValue === 'title' &&
+			!book.getAttribute('data-title').includes(value)
+		) {
+			book.style.display = 'none';
+		} else if (
+			searchKeyValue === 'author' &&
+			!book.getAttribute('data-author').includes(value)
+		) {
+			book.style.display = 'none';
+		} else if (
+			searchKeyValue === 'submitter' &&
+			!book.getAttribute('data-submitter').includes(value)
+		) {
+			book.style.display = 'none';
+		} else {
+			book.style.display = '';
+		}
+	});
+};
+
 // Call the function immediately with the current select value
 checkSelectValue(selectElement.value);
 
 selectElement.addEventListener('change', (event) => {
 	checkSelectValue(event.target.value);
+});
+
+recommendationSearchBar.addEventListener('input', (event) => {
+	filterRecommendations(event.target.value);
 });
