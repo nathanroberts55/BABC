9ba6f7dbb69649a45ebaa158b382f5b9eb030cbc
Nate R <45110045+nathanroberts55@users.noreply.github.com>
2023-12-27 09:40:59 -0500

    Updated the CSRF Authentication (#44)
    * Updated the CSRF Authentication
    
    * Removed Decorator import

--- api/views.py
+++ api/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 from books.models import Book
+from .auth import CsrfExemptSessionAuthentication
 from .serializers import BookSerializer, CreateBookSerializer, UserSerializer
 
 
@@ -34,8 +34,8 @@ class SingleBookView(generics.RetrieveAPIView):
         return {"request": self.request}
 
 
-@method_decorator(csrf_exempt, name="dispatch")
 class CreateBookView(generics.CreateAPIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     serializer_class = CreateBookSerializer
 
     def create(self, request, *args, **kwargs):
