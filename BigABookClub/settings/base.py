11a9a02b9267a2ace3f2c614759bdbf81286917b
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-11-25 19:04:15 -0500

    Fix for NPM BIN PATH

--- BigABookClub/settings/base.py
+++ BigABookClub/settings/base.py
@@ -157,4 +157,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-NPM_BIN_PATH = "C:\Program Files\\nodejs\\npm.cmd"
+NPM_BIN_PATH = (
+    "C:\Program Files\\nodejs\\npm.cmd"
+    if os.getenv("DEBUG") == 1
+    else "/usr/local/bin/npm"
+)
